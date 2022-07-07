fcd054e3c631327927e8e36ef7d774fb8f46227c
egorchuzhavko <egorchuzhavko@gmail.com>
2022-07-08 00:35:37 +0300

    new com

---
+++ c#/Isograms/Isograms/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(IsIsogram("Dermatoglyphics"));
+
+static bool IsIsogram(string str)
+{
+    int len = str.Length;
+    return str.Where(c => Char.IsLetter(c)).Select(q => Char.ToLower(q)).Distinct().Count() == len;
+}
