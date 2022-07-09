83e9112b3ea1c1f7b3be3f5da3c27eb8c82a36c6
egorchuzhavko <egorchuzhavko@gmail.com>
2022-07-09 18:40:58 +0300

    update

---
+++ c#/FriendOrFoe/FriendOrFoe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static IEnumerable<string> FriendOrFoe(string[] names) => names.Where(x => x.Length == 4);
+
+    public static void Main()
+    {
+        var items = Kata.FriendOrFoe(new string[] { "Ryan", "Kieran", "Mark", "Jimmy" });
+        foreach (var item in items) Console.WriteLine(item);
+    }
+}
